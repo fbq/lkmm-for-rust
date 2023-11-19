2a08aef4223be8870bb49c95675443492b35c7bf
Boqun Feng <boqun.feng@gmail.com>
2023-11-19 09:03:34 -0800

    Add mp tests and fix LB exist clause
    Signed-off-by: Boqun Feng <boqun.feng@gmail.com>

--- litmus-tests/src/lib.rs
+++ litmus-tests/src/lib.rs
@@ -1,6 +1,7 @@
 use core::ptr::write_volatile;
 use core::cell::UnsafeCell;
 use core::arch::asm;
+use core::ptr::NonNull;
 
 // Simulates shared memory used for C <-> Rust communication.
 pub struct SharedMem(UnsafeCell<usize>);
@@ -19,25 +20,63 @@ impl SharedMem {
     /// Full barrier.
     ///
     /// C version: smp_mb()
-    #[cfg(target_arch = "aarch64")]
     pub fn smp_mb() {
+        #[cfg(target_arch = "aarch64")]
         // C version: asm volatile("dmbish": : : "memory");
         unsafe { asm!("dmb ish"); }
-    }
-    #[cfg(target_arch = "x86_64")]
-    pub fn smp_mb() {
-        // C version: asm volatile("dmbish": : : "memory");
+
+        #[cfg(target_arch = "x86_64")]
+        // C version: asm volatile("mfence": : : "memory");
         unsafe { asm!("mfence"); }
     }
 
+    /// A release store
+    ///
+    /// C version: smp_store_release()
+    pub fn smp_store_release(&self, val: usize) {
+        #[cfg(target_arch = "aarch64")]
+        unsafe { asm!("stlr {val} [{ptr}]", val = in(reg) val, ptr = in(reg) self.0.get()); }
+
+
+        #[cfg(target_arch = "x86_64")]
+        // C version:
+        // asm volatile("": : : "memory");
+        // WRITE_ONCE(...);
+        {
+            unsafe { asm!(""); }
+            self.write_once(val);
+        }
+    }
     pub fn new(val: usize) -> Self {
         SharedMem(UnsafeCell::new(val))
     }
 }
 
-
 // According to LKMM, read_once and write_once are volatile atomic.
 unsafe impl Sync for SharedMem {}
+
+pub struct RcuPtr<T> {
+    ptr: SharedMem,
+    phantom: core::marker::PhantomData<*const T>
+}
+
+impl<T> RcuPtr<T> {
+    /// Address dependency carrying load
+    ///
+    /// C version: rcu_dereference()
+    ///
+    /// In fact the latest LKMM request all READ_ONCE()s can carry address dependencies.
+    pub fn rcu_dereference(&self) -> Option<&T> {
+        let ptr = self.ptr.read_once() as *mut T;
+
+        Some(unsafe { NonNull::new(ptr)?.as_ref() })
+    }
+
+    pub fn rcu_assign_pointer(&self, ptr: *mut T) {
+        self.ptr.smp_store_release(ptr as usize);
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -68,8 +107,8 @@ mod tests {
             p0.join().unwrap();
             let (r0, r1) = p1.join().unwrap();
 
-            // Result: Never
             // exists (1:r0=1 /\ 1:r1=0)
+            // Result: Never
             //
             // expect r0 == 1 && r1 == 0 never happens
             assert!(!(r0 == 1 && r1 == 0));
@@ -110,12 +149,47 @@ mod tests {
             let p0_r0 = p0.join().unwrap();
             let p1_r0 = p1.join().unwrap();
 
-            // Result: Never
             // exists (0:r0=1 /\ 1:r0=1)
+            // Result: Never
             //
             // expect p0_r0 == 1 && p1_r0 == 0 never happens
-            assert!(!(p0_r0 == 1 && p1_r0 == 0));
+            assert!(!(p0_r0 == 1 && p1_r0 == 1));
         });
     }
 
+    #[test]
+    fn mp_poonceonces() {
+        // C litmus test:
+        // tools/memory-model/litmus-tests/MP+poonceonces.litmus
+
+        let buf_in_mem = SharedMem::new(0);
+        let flag_in_mem = SharedMem::new(0);
+
+        thread::scope(|scope| {
+            let buf = &buf_in_mem;
+            let flag = &flag_in_mem;
+
+            let p0 = scope.spawn(move || {
+                buf.write_once(1);
+                flag.write_once(1);
+            });
+
+            let p1 = scope.spawn(move || -> (usize, usize) {
+                let r0 = flag.read_once();
+                let r1 = buf.read_once();
+
+                (r0, r1)
+            });
+
+            p0.join().unwrap();
+
+            let (r0, r1) = p1.join().unwrap();
+
+            // exists (1:r0=1 /\ 1:r1=0)
+            // Result: Sometimes
+            //
+            // expect r0 == 1 && r1 == 0 may happen
+            println!("MP+poonceonces: r0 == {} r1 == {}", r0, r1);
+        });
+    }
 }
